5070158e595ac82c8a74d6779635ec425c883c19
Eskelai <eskelai.saite@gmail.com>
2019-10-21 17:44:46 +0400

    Initial setup

---
+++ modules/task_1/tonkov_a_min_matrix/min_matrix.cpp
@@ -0,0 +1,9 @@
+// Copyright 2019 Tonkov Alexei
+#include <algorithm>
+#include <vector>
+#include <mpi.h>
+#include <random>
+#include <ctime>
+#include "./min_matrix.h"
+
+
---
+++ modules/task_1/tonkov_a_min_matrix/min_matrix.h
@@ -0,0 +1,9 @@
+// Copyright 2019 Tonkov Alexei
+#ifndef MODULES_TASK_1_TONKOV_V_MIN_MATRIX_MIN_MATRIX_H_
+#define MODULES_TASK_1_TONKOV_V_MIN_MATRIX_MIN_MATRIX_H_
+
+#include <mpi.h>
+#include <vector>
+
+
+#endif  // MODULES_TASK_1_TONKOV_V_MIN_MATRIX_MIN_MATRIX_H_
