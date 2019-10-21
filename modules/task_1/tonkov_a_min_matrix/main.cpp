c6df6acb040683a5d982077342f24bc05365c746
Eskelai <eskelai.saite@gmail.com>
2019-10-21 18:52:01 +0400

    Fix header + Add tests

--- modules/task_1/tonkov_a_min_matrix/main.cpp
+++ modules/task_1/tonkov_a_min_matrix/main.cpp
@@ -5,6 +5,43 @@
 #include <vector>
 #include "./min_matrix.h"
 
+TEST(Matrix_Min_MPI, Test_Min_Rnd_Matrix) {
+	int rank;
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	std::vector<int> matrix;
+	const int rows = 10;
+	const int columns = 10;
+
+	if (rank == 0) {
+		matrix = getMatrixRandom(rows, columns);
+	}
+
+	int min;
+	min = getMinMatrix(matrix, rows, columns);
+
+	if (rank == 0) {
+		int answer = *std::min_element(matrix.begin(), matrix.end());
+		ASSERT_EQ(min, answer);
+	}
+}
+
+TEST(Matrix_Min_MPI, Test_With_Cicle_Matrix) {
+	int rank;
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	std::vector<int> matrix;
+	const int rows = 100;
+	const int columns = 100;
+
+	if (rank == 0) {
+		matrix = getMatrixCicle(rows, columns);
+	}
+
+	int min_matrix = getMinMatrix(matrix, rows, columns);
+	if (rank == 0) {
+		ASSERT_EQ(0, min_matrix);
+	}
+}
+
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
   MPI_Init(&argc, &argv);
--- modules/task_1/tonkov_a_min_matrix/min_matrix.h
+++ modules/task_1/tonkov_a_min_matrix/min_matrix.h
@@ -7,5 +7,6 @@
 
 std::vector<int> getMatrixCicle(int rows, int columns);
 std::vector<int> getMatrixRandom(int rows, int columns);
+int getMinMatrix(const std::vector<int> matrix, int rows, int cols);
 
 #endif  // MODULES_TASK_1_TONKOV_A_MIN_MATRIX_MIN_MATRIX_H_
