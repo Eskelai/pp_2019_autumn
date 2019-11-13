3a9c88b7456ddf2a046e2ee90d0c4d5f37f0deeb
Eskelai <eskelai.saite@gmail.com>
2019-11-13 15:18:35 +0400

    Initial test setup

---
+++ modules/task_2/tonkov_a_producer_consumer/min_matrix.h
@@ -0,0 +1,13 @@
+// Copyright 2019 Tonkov Alexei
+#ifndef MODULES_TASK_1_TONKOV_A_MIN_MATRIX_MIN_MATRIX_H_
+#define MODULES_TASK_1_TONKOV_A_MIN_MATRIX_MIN_MATRIX_H_
+
+#include <mpi.h>
+#include <vector>
+
+std::vector<int> getMatrixCicle(int rows, int columns);
+std::vector<int> getMatrixRandom(int rows, int columns);
+int getMinMatrix(const std::vector<int> matrix, int rows, int columns);
+
+
+#endif  // MODULES_TASK_1_TONKOV_A_MIN_MATRIX_MIN_MATRIX_H_
