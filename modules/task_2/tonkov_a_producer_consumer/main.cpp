e66f816e481873c2f9470ba535f89ef5ef8c5722
Eskelai <eskelai.saite@gmail.com>
2019-11-13 15:31:44 +0400

    File architecture for 2 task

--- modules/task_2/tonkov_a_producer_consumer/main.cpp
+++ modules/task_2/tonkov_a_producer_consumer/main.cpp
@@ -1,75 +1,7 @@
 // Copyright 2019 Tonkov Alexei
 #include <gtest-mpi-listener.hpp>
 #include <gtest/gtest.h>
-#include <algorithm>
-#include <vector>
-#include "../../../modules/task_1/tonkov_a_min_matrix/min_matrix.h"
-
-TEST(Matrix_Min_MPI, Test_With_Given_Matrix) {
-    int rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    const int rows = 2;
-    const int columns = 2;
-    std::vector<int> matrix(rows * columns);
-    matrix[0] = 6;
-    matrix[1] = 2;
-    matrix[2] = 8;
-    matrix[3] = 3;
-
-    int min = getMinMatrix(matrix, rows, columns);
-    if (rank == 0) {
-        ASSERT_EQ(2, min);
-    }
-}
-
-TEST(Matrix_Min_MPI, Test_Min_Rnd_Matrix) {
-    int rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    std::vector<int> matrix;
-    const int rows = 10;
-    const int columns = 10;
-
-    if (rank == 0) {
-        matrix = getMatrixRandom(rows, columns);
-    }
-
-    int min;
-    min = getMinMatrix(matrix, rows, columns);
-
-    if (rank == 0) {
-        int answer = *std::min_element(matrix.begin(), matrix.end());
-        ASSERT_EQ(min, answer);
-    }
-}
-
-TEST(Matrix_Min_MPI, Test_With_Cicle_Matrix) {
-    int rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    std::vector<int> matrix;
-    const int rows = 100;
-    const int columns = 100;
-
-    if (rank == 0) {
-        matrix = getMatrixCicle(rows, columns);
-    }
-
-    int min_matrix = getMinMatrix(matrix, rows, columns);
-    if (rank == 0) {
-        ASSERT_EQ(0, min_matrix);
-    }
-}
-
-TEST(Matrix_Min_MPI, Test_With_Zero_Size_Matrix) {
-    int rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    std::vector<int> matrix;
-    const int rows = 10;
-    const int columns = 0;
-
-    if (rank == 0) {
-        ASSERT_ANY_THROW(getMatrixRandom(rows, columns));
-    }
-}
+#include "../../../modules/task_2/tonkov_a_producer_consumer/producer_consumer.h"
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
