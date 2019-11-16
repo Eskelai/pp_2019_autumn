361891b3e3d70ffae185666f9483da4da324dfad
Alexei Tonkov <42972237+Eskelai@users.noreply.github.com>
2019-11-16 17:36:33 +0300

    Revert "Tonkov test"

--- modules/task_2/tonkov_a_producer_consumer/producer_consumer.cpp
+++
@@ -1,5 +0,0 @@
-// Copyright 2019 Tonkov Alexei
-#include <mpi.h>
-#include "../../../modules/task_2/tonkov_a_producer_consumer/producer_consumer.h"
-
-
--- modules/task_2/tonkov_a_producer_consumer/producer_consumer.h
+++
@@ -1,9 +0,0 @@
-// Copyright 2019 Tonkov Alexei
-#ifndef MODULES_TASK_2_TONKOV_A_PRODUCER_CONSUMER_H_
-#define MODULES_TASK_2_TONKOV_A_PRODUCER_CONSUMER_H_
-
-#include <mpi.h>
-
-int getMinMatrix(const std::vector<int> matrix, int rows, int columns);
-
-#endif  // MODULES_TASK_2_TONKOV_A_PRODUCER_CONSUMER_H_
