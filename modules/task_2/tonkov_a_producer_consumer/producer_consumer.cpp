cce54a11db5674a99b1db2fbfb436352fc414c75
Eskelai <eskelai.saite@gmail.com>
2019-11-22 18:42:25 +0400

    MACOS test + removed comments

--- modules/task_2/tonkov_a_producer_consumer/producer_consumer.cpp
+++ modules/task_2/tonkov_a_producer_consumer/producer_consumer.cpp
@@ -43,13 +43,11 @@ void produceConsume(int* numbers, double* roots, int MAX_NUMBERS) {
                 roots[status.MPI_TAG - 1] = root;
             }
 
-            // printf("[RANK:0] Sending %i to RANK: %i, from index %i.\n", numbers[nextnum - 1], status.MPI_SOURCE,
-            //                                                            nextnum);
             MPI_Send(&(numbers[nextnum - 1]), 1, MPI_INT, status.MPI_SOURCE,
                 nextnum, MPI_COMM_WORLD);  // Sends nextnum as a tag (MPI_TAG = Number of job)
         }
 
-        // Send termination signal to each rank when they submit their last job
+        // Send termination signal after getting last job
         int num_terminated = 0;
         for (num_terminated = 0; num_terminated < size - 1; num_terminated++) {
             // Wait for a worker to become available
@@ -62,7 +60,6 @@ void produceConsume(int* numbers, double* roots, int MAX_NUMBERS) {
             if (status.MPI_TAG > 0) {
                 roots[status.MPI_TAG - 1] = root;
             }
-            // printf("[RANK:0] Terminating rank %i.\n", status.MPI_SOURCE);
             // Send termination signal (tag = 0)
             MPI_Send(&num_terminated, 1, MPI_INT, status.MPI_SOURCE,
                 0, MPI_COMM_WORLD);
@@ -78,24 +75,14 @@ void produceConsume(int* numbers, double* roots, int MAX_NUMBERS) {
             int num = 0;
             MPI_Status status;
             MPI_Recv(&num, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-            // printf("[%i] Received job: %i [tag = %i].\n", rank, num, status.MPI_TAG);
             if (status.MPI_TAG == 0) {
                 terminated = true;
-                // printf("[%i] Terminated.\n", rank);
             } else {
                 root = sqrt(num);
-                // printf("[%i] Submitting result: sqrt(%i) = %f.\n", rank, num, root);
                 MPI_Send(&root, 1, MPI_DOUBLE, 0, status.MPI_TAG, MPI_COMM_WORLD);
             }
         } while (!terminated);
     }
 
     MPI_Barrier(MPI_COMM_WORLD);
-
-    if (rank == 0) {
-        // printf("\n\nResults:\n");
-        for (int i = 0; i < number_amount; i++) {
-            // printf("sqrt(%i) = %f\n", numbers[i], roots[i]);
-        }
-    }
 }
