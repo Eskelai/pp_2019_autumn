dd531cb46f04af63cb04123bee7469fc941a0093
Eskelai <eskelai.saite@gmail.com>
2019-11-25 01:43:04 +0400

    Fix

--- modules/task_2/tonkov_a_producer_consumer/main.cpp
+++ modules/task_2/tonkov_a_producer_consumer/main.cpp
@@ -51,7 +51,7 @@ TEST(Producer_Consumer_MPI, test_main_func_true) {
     int numbers[4] = { 4, 16, 9, 36 };
     double roots[4];
     double real_roots[4];
-    rootsFromNumbers(numbers, roots, 4);
+    rootsFromNumbers(numbers, real_roots, 4);
 
     produceConsume(numbers, roots, 4);
 
